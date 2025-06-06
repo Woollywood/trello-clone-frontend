f0f11fa7d82274932631ad125963f815da17572c
a.cherkashin <a.cherkashin@notamedia.com.ru>
2025-06-07 01:16:20 +0300

    feat: refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import { cn } from '@/lib/utils';
+import { cn } from '@/libs/shadcn/utils';
 
 import './globals.css';
 
